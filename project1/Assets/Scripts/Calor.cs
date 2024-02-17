49cffbbd3dd44ea0de0b99a846848ffd84495d30
Student8 <Student8@LAPTOP-ANTH0S88>
2024-02-17 16:03:21 +0300

    color chanded

--- project1/Assets/Scripts/Calor.cs
+++ project1/Assets/Scripts/Calor.cs
@@ -8,9 +8,29 @@ public class Calor : MonoBehaviour
     [SerializeField]
     private MeshRenderer _calor;
 
+    [SerializeField] private bool _diffcolor;
+
     // Start is called before the first frame update
     void Start()
     {
-        _calor.material.color = new Color( 0.5f, 0, 0);
+        _calor = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        if (_diffcolor)
+        {
+            var green = Mathf.Sign(Time.time);
+            var red = Mathf.Sin(Time.time);
+            var blue = Mathf.Cos(Time.time);
+            _calor.material.color = new Color(red, green, blue);
+        }
+        else
+        {
+            
+        }
     }
 }
+
+
+
