189caa6a90b0e1a89e0563a742a2aeeeb09c9330
Student6 <Student6@LAPTOP-3IUJ048E>
2024-04-06 15:04:38 +0300

    restart

---
+++ project1/Assets/Scenes/Test/New Folder/ypal.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+
+public class ypal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
