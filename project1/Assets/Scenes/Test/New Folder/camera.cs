98f52db4952f3d873f2833f272469a6fed0fae9f
Student6 <Student6@LAPTOP-3IUJ048E>
2024-04-13 14:47:37 +0300

    camera

---
+++ project1/Assets/Scenes/Test/New Folder/camera.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class camera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    public float rotSpeed = 1.5f; //скорость вращения
+    public float zoomSpeed = 5.0f; //скорость приближения камеры
+    private float _rotY, _rotX; //углы по x и y
+    private Vector3 _offset; //смещение камеры относительно объекта
+
+
+    // Use this for initialization
+    void Start()
+    {
+        _rotY = transform.eulerAngles.y;
+        _rotX = transform.eulerAngles.x;
+        _offset = target.position - transform.position; //получает начальное смещение
+    }
+
+    void LateUpdate()
+    {
+        float input = Input.GetAxis("Mouse ScrollWheel");
+        if (input != 0) //если крутится колесико мыши
+        {
+            Camera.main.fieldOfView += input * zoomSpeed; //зум
+
+            //сперва хотел менять смещение, что то криво вышло
+            //_offset.x += 10 * input;
+            //_offset.y += 10 * input;
+        }
+
+        _rotY -= Input.GetAxis("Mouse X") * rotSpeed; //поворот камеры вокруг объекта
+        _rotX += Input.GetAxis("Mouse Y") * rotSpeed;
+
+        if (_rotY != 0 || _rotX != 0)
+        {
+            Quaternion rotation = Quaternion.Euler(_rotX, _rotY, 0); //задает вращение камеры
+            transform.position = target.position - (rotation * _offset);
+            transform.LookAt(target);
+        }
+    }
+}
+
